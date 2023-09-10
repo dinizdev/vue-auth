b2baf335af1a6913b937b8aecb37a7da8b1f9214
dinizdev <dinizdevmaster@gmail.com>
2023-09-09 23:51:00 -0300

    fix:register url

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -42,8 +42,8 @@ const register = () => {
             <p v-if="errMsg" class="text-red-500"> {{ errMsg }}</p>
             <button type="submit" @click="register"
                 class="p-2 font-bold uppercase bg-green-500 text-black pointer hover:bg-green-600">Login</button>
-            <router-link to="/register">
-                <a class="text-green-300 text-center pointer underline">Sign Up - Register Here</a>
+            <router-link to="/register" class="text-center">
+                <a class="text-green-300 pointer underline">Sign Up - Register Here</a>
             </router-link>
         </form>
         <p class=""> version 1.0</p>
